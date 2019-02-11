3adbc36cafd5183cad2f9f8de28b499350f2e967
ALP <alpetteruti@gmail.com>
2019-02-11 17:57:27 -0500

    Updates

---
+++ precache-manifest.5d94e79bf62ea3233136efb54a8b763d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82b693316d48df050700",
+    "url": "/bradygame/static/js/runtime~main.82b69331.js"
+  },
+  {
+    "revision": "1ff1a285d5a664bb60f5",
+    "url": "/bradygame/static/js/main.1ff1a285.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/bradygame/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "1ff1a285d5a664bb60f5",
+    "url": "/bradygame/static/css/main.1ffd7d12.chunk.css"
+  },
+  {
+    "revision": "9b011fab3af4c176a3546680aa5dae04",
+    "url": "/bradygame/index.html"
+  }
+];
